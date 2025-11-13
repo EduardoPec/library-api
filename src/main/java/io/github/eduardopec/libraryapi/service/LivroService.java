a520ec39e1773de997a88547b70c9564b06d51ee
EduardoPec <eduardopecanha05@gmail.com>
2025-11-12 21:24:20 -0300

    feat(service): regras de negócio de Autor e Livro

---
+++ src/main/java/io/github/eduardopec/libraryapi/service/LivroService.java
@@ -0,0 +1,76 @@
+package io.github.eduardopec.libraryapi.service;
+
+import io.github.eduardopec.libraryapi.model.GeneroLivro;
+import io.github.eduardopec.libraryapi.model.Livro;
+import io.github.eduardopec.libraryapi.repository.LivroRepository;
+import io.github.eduardopec.libraryapi.validator.LivroValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static io.github.eduardopec.libraryapi.repository.specs.LivroSpecs.*;
+
+@Service
+@RequiredArgsConstructor
+public class LivroService {
+
+    private final LivroRepository repository;
+    private final LivroValidator validator;
+
+    public Livro salvar(Livro livro) {
+        validator.validar(livro);
+        return repository.save(livro);
+    }
+
+    public Optional<Livro> obterPorId(UUID id) {
+        return repository.findById(id);
+    }
+
+    public void deletar(Livro livro) {
+        repository.delete(livro);
+    }
+
+    public Page<Livro> pesquisa(String isbn,
+                                String titulo,
+                                String nomeAutor,
+                                GeneroLivro genero,
+                                Integer anoPublicacao,
+                                Integer pagina,
+                                Integer tamanhoPagina){
+
+        Specification<Livro> specs = Specification.allOf();
+
+        if (isbn != null) {
+            specs = specs.and(isbnEqual(isbn));
+        }
+        if (titulo != null) {
+            specs = specs.and(tituloLike(titulo));
+        }
+        if (genero != null) {
+            specs = specs.and(generoEqual(genero));
+        }
+        if (anoPublicacao != null) {
+            specs = specs.and(anoPublicacaoEqual(anoPublicacao));
+        }
+        if (nomeAutor != null) {
+            specs = specs.and(nomeAutorLike(nomeAutor));
+        }
+
+        Pageable pagerequest = PageRequest.of(pagina, tamanhoPagina);
+        return repository.findAll(specs, pagerequest);
+    }
+
+    public void atualizar(Livro livro) {
+        if (livro.getId() == null) {
+            throw new IllegalArgumentException("Para atualizar, é necessário que o livro ja esteja salvo na base");
+        }
+        validator.validar(livro);
+        repository.save(livro);
+    }
+}
