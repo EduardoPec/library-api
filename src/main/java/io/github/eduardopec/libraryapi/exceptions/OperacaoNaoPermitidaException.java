0b894ab2994952ba4f186e952a7bea094a5b5bbb
EduardoPec <eduardopecanha05@gmail.com>
2025-11-12 21:23:35 -0300

    feat(error): exceções de domínio

---
+++ src/main/java/io/github/eduardopec/libraryapi/exceptions/OperacaoNaoPermitidaException.java
@@ -0,0 +1,8 @@
+package io.github.eduardopec.libraryapi.exceptions;
+
+public class OperacaoNaoPermitidaException extends RuntimeException {
+
+    public OperacaoNaoPermitidaException(String message) {
+        super(message);
+    }
+}
